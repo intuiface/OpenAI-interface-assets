a62d1327769cc87080f7ba42b0b5545b3b8461ed
meunier <meunier@intuiface.com>
2024-01-23 15:10:34 -0600

    Update wording and handle errors

--- ChatGPT4TS/src/ChatGPT4TS.ts
+++ ChatGPT4TS/src/ChatGPT4TS.ts
@@ -2,8 +2,7 @@ import { Action, Asset, IntuifaceElement, Parameter, Property, Trigger } from '@
 import { Message } from './Message';
 
 
-class rawMessage
-{
+class rawMessage {
     role: string;
     content: string;
 }
@@ -18,8 +17,7 @@ class rawMessage
     category: 'OpenAI',
     behaviors: [],
 })
-export class ChatGPT4TS extends IntuifaceElement 
-{
+export class ChatGPT4TS extends IntuifaceElement {
 
     //#region Public Properties
 
@@ -52,8 +50,7 @@ export class ChatGPT4TS extends IntuifaceElement
     /**
      * @constructor
      */
-    public constructor()
-    {
+    public constructor() {
         super();
     }
 
@@ -62,20 +59,36 @@ export class ChatGPT4TS extends IntuifaceElement
     //#region Triggers
 
     /**
-     * Trigger Example
+     * Trigger Response Received
      */
     @Trigger({
         name: 'responseReceived',
         displayName: 'Response received',
         description: 'Raised when GPT API has responded'
     })
-    public responseReceived( @Parameter({
+    public responseReceived(@Parameter({
         name: 'response',
         displayName: 'Response',
         description: 'The response received from OpenAI',
         defaultValue: '',
         type: String
     }) response: string): void { }
+
+    /**
+     * Trigger Errror Received
+     */
+    @Trigger({
+        name: 'errorReceived',
+        displayName: 'Error message received',
+        description: 'Raised when GPT API has responded with an error.'
+    })
+    public errorReceived(@Parameter({
+        name: 'errorMessage',
+        displayName: 'Error message',
+        description: 'The error message received from OpenAI',
+        defaultValue: '',
+        type: String
+    }) errorMessage: string): void { }
 
     //#endregion Triggers
 
@@ -86,12 +99,11 @@ export class ChatGPT4TS extends IntuifaceElement
         *Clears the chat history to start a new session
         */
     @Action({
-        displayName: 'Clear Chat Session',
-        description: 'Clears the chat history.',
+        displayName: 'Clear chat session',
+        description: 'Clears the existing chat session. The next ‘Send prompt’ will start a new chat.',
         validate: true
     })
-    public clearChatSession()
-    {
+    public clearChatSession() {
         this.listMessages = [];
     }
 
@@ -99,11 +111,18 @@ export class ChatGPT4TS extends IntuifaceElement
      * Ask GPT4 for a chat completion
      */
     @Action({
-        displayName: 'Text Completion',
-        description: 'Sends a request to OpenAI to complete text.',
+        displayName: 'Send Prompt',
+        description: 'Sends a prompt to the GPT chat model you specify.',
         validate: true
     })
     public async textCompletion(
+        @Parameter({
+            name: 'message',
+            displayName: 'Prompt',
+            description: 'The user message to send to OpenAI',
+            defaultValue: '',
+            type: String
+        }) message: string,
         @Parameter({
             name: 'model',
             displayName: 'Model',
@@ -112,87 +131,92 @@ export class ChatGPT4TS extends IntuifaceElement
             type: String
         }) model: string,
         @Parameter({
-            name: 'message',
-            displayName: 'message',
-            description: 'The message to send to OpenAI',
-            defaultValue: '',
-            type: String
-        }) message: string,
-        @Parameter({
-            name: '',
-            displayName: 'temperature',
+            name: 'temperature',
+            displayName: 'Temperature',
             description: '',
-            defaultValue: '1',
+            defaultValue: 1,
             type: Number
         }) temperature: number,
         @Parameter({
             name: 'maxTokens',
             displayName: 'Maximum Tokens',
-            description: 'The max tokens to use with OpenAI API',
+            description: 'The maximum number of tokens to use in a request',
             defaultValue: 20,
             type: Number
-        }) maxTokens: number): Promise<void>
-    {
-        const req = 'https://api.openai.com/v1/chat/completions';
+        }) maxTokens: number): Promise<void> {
 
-        const headers: HeadersInit = {
-            'Authorization': `Bearer ` + this.apiKey,
-            'Content-Type': `application/json`
+        try {
+
+
+            const req = 'https://api.openai.com/v1/chat/completions';
+
+            const headers: HeadersInit = {
+                'Authorization': `Bearer ` + this.apiKey,
+                'Content-Type': `application/json`
+            }
+
+            const userMessage = new Message();
+            userMessage.role = 'user';
+            userMessage.content = message;
+            this.listMessages.push(userMessage);
+
+            //create array of raw Messages (not Intuiface Class) to send to the API
+            var rawListMessage: rawMessage[] = [];
+            for (var i in this.listMessages) {
+                const m = new rawMessage();
+                m.role = this.listMessages[i].role;
+                m.content = this.listMessages[i].content;
+                rawListMessage.push(m);
+            }
+
+
+            const body = {
+                'model': model,
+                'messages': rawListMessage,
+                'max_tokens': maxTokens,
+                'temperature': temperature
+            }
+
+            const opts: RequestInit = {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body)
+            };
+
+            const rawResponse = await fetch(req, opts);
+
+
+           
+
+            const json = await rawResponse.json();
+
+            var response: string = '';
+
+            //Catch error response from API 
+            if(rawResponse.status != 200){
+                this.errorReceived(json.error.message);
+                return null;
+            }
+
+            //If we wanted to consider all choices returned by OpenAI
+            //for (var i in json.choices)
+
+            //For now, only consider the first response returned by OpenAI
+
+            if (json.choices && json.choices.length > 0) {
+                var c = new Message();
+                c.content = json.choices[0].message.content.replaceAll('\n', '');
+                c.role = json.choices[0].message.role;
+                response = c.content;
+                this.listMessages.push(c);
+
+                this.notifyPropertyChanged('listMessages', this.listMessages);
+                this.responseReceived(response);
+            }
+        } catch (error) {
+            //raise error received trigger
+            this.errorReceived(error.message);
         }
-
-        const userMessage = new Message();
-        userMessage.role = 'user';
-        userMessage.content = message;
-        this.listMessages.push(userMessage);
-
-        //create array of raw Messages (not Intuiface Class) to send to the API
-        var rawListMessage: rawMessage[] = [];
-        for (var i in this.listMessages)
-        {
-            const m = new rawMessage();
-            m.role = this.listMessages[i].role;
-            m.content = this.listMessages[i].content;
-            rawListMessage.push(m);
-        }
-
-
-        const body = {
-            'model': model,
-            'messages': rawListMessage,
-            'max_tokens': maxTokens,
-            'temperature': temperature
-        }
-
-        const opts: RequestInit = {
-            method: 'POST',
-            headers,
-            body: JSON.stringify(body)
-        };
-
-        const rawResponse = await fetch(req, opts);
-
-        const json = await rawResponse.json();
-
-        var response:string = '';
-
-
-        //If we wanted to consider all choices returned by OpenAI
-        //for (var i in json.choices)
-
-        //For now, only consider the first response returned by OpenAI
-
-        if (json.choices.length > 0)
-        {
-            var c = new Message();
-            c.content = json.choices[0].message.content.replaceAll('\n', '');
-            c.role = json.choices[0].message.role;
-            response = c.content;
-            this.listMessages.push(c);
-        }
-
-
-        this.notifyPropertyChanged('listMessages', this.listMessages);
-        this.responseReceived(response);
     }
 
 
@@ -200,19 +224,18 @@ export class ChatGPT4TS extends IntuifaceElement
    * Add a System message in the conversation
    */
     @Action({
-        displayName: 'Add System Message',
-        description: 'Inserts a system message in the current conversation.',
+        displayName: 'Specify system guidance',
+        description: 'Adds a system message to every ‘Send prompt’.',
         validate: true
     })
     public addSystemMessage(
         @Parameter({
             name: 'message',
-            displayName: 'message',
-            description: 'The message to send to OpenAI',
+            displayName: 'Message',
+            description: 'The system message to send to OpenAI',
             defaultValue: '',
             type: String
-        }) message: string): void
-    {
+        }) message: string): void {
         const sysMessage = new Message();
         sysMessage.role = 'system';
         sysMessage.content = message;
